057a7521036c54cdeb7ec859728f8dde0df7148a
Antonio Cvitkovic <125733906+ancvitko@users.noreply.github.com>
2024-10-08 20:33:35 +0200

    Changed up gitignore, accidently ignored important files

---
+++ src/res/dep/passives.py
@@ -0,0 +1,26 @@
+PASSIVES = {
+    'Dragon Slayer': {
+        "description": "Increased damage against dragons.",
+        "value": "5%",
+    },
+    'Beast Slayer': {
+        "description": "Increased damage against beasts.",
+        "value": "5%",
+    },
+    'Undead Slayer': {
+        "description": "Increased damage against undead.",
+        "value": "5%",
+    },
+    'Giant Slayer': {
+        "description": "Increased damage against giants.",
+        "value": "5%",
+    },
+    'Elemental Slayer': {
+        "description": "Increased damage against elementals.",
+        "value": "5%",
+    },
+    'Deadly Precision' : {
+        "description": "Increased critical strike chance.",
+        "value": "3%",
+    },
+}
